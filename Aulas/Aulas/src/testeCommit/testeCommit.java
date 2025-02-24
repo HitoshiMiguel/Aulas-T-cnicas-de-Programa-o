3a7264ec9749e06bf1c38f5ac58c4aaf98e37840
migue <migue@Notebook_Miguel>
2025-02-24 10:52:31 -0300

    teste soma

--- Aulas/Aulas/src/testeCommit/testeCommit.java
+++ Aulas/Aulas/src/testeCommit/testeCommit.java
@@ -6,7 +6,16 @@ public class testeCommit {
 	Scanner ler = new Scanner(System.in);
 	
 	System.out.println("Olá mundo!! Teste para o gihub!");
-
+	
+	int x, y;
+	double soma;
+	System.out.println("Digite um número para X:");
+	x = ler.nextInt();
+	System.out.println("Digite um número para Y:");
+	y = ler.nextInt();
+	
+	soma = x + y;
+	System.out.println(soma);
 	}
 
 }
