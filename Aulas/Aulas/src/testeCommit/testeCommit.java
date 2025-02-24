49aa5e31173bcdcef86ad5c7a8144bcf6f1940e6
migue <migue@Notebook_Miguel>
2025-02-24 10:44:02 -0300

    teste3

---
+++ Aulas/Aulas/src/testeCommit/testeCommit.java
@@ -0,0 +1,12 @@
+package testeCommit;
+import java.util.Scanner;
+public class testeCommit {
+
+	public static void main(String[] args) {
+	Scanner ler = new Scanner(System.in);
+	
+	System.out.println("Olá mundo! Teste para o gihub!");
+
+	}
+
+}
